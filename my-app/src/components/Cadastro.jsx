9a559cd0e965fd4cdafdb06aecfb0e11de70c9f6
João Oliveira <joaopedro.santosoliveira2003@gmail.com>
2024-09-29 17:36:45 -0300

    FIN-36 - integrating login and register with backend

--- my-app/src/components/Cadastro.jsx
+++ my-app/src/components/Cadastro.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { useNavigate} from 'react-router-dom';
-
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
+import { Grid, Box, Button, TextField, Typography, Paper } from '@mui/material';
+import Logo from '../img/LOGO.png';
+import { register } from "../controllers/registerController";
 
 /**
  * Função que retorna o JSX da página de Cadastro
@@ -9,69 +11,118 @@ import { useNavigate} from 'react-router-dom';
  */
 function Cadastro() {
     const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     // Função para o botão "Cadastrar"
     const cadastrar = () => {
-        // const nome = document.getElementById('nome').value;
-        // const email = document.getElementById('email').value;
-        // const senha = document.getElementById('senha').value;
-        // const confirmarSenha = document.getElementById('confirmar-senha').value;
-
-        // if (nome && email && senha && confirmarSenha) {
-        //     if (senha === confirmarSenha) {
-        //         alert('Cadastro bem-sucedido!');
-        //     } else {
-        //         alert('As senhas não coincidem. Por favor, tente novamente.');
-        //     }
-        // } else {
-        //     alert('Por favor, preencha todos os campos.');
-        // }
-
-        navigate('/')
+        navigate('/');
     };
 
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // Evita o reload da página
+    
+        try {
+          // Chama a função login e envia os dados
+          const token = await register(name, email, password);
+          navigate('/');
+    
+          // Lógica adicional após o login bem-sucedido
+          console.log('Login bem-sucedido, token:', token);
+        } catch (error) {
+          console.error("Email ou senha incorretos!");
+        }}
+
     return (
-        <div className="container">
-            {/* Seção de cadastro */}
-            <div className="secao-cadastro">
-                <h2>Cadastre-se</h2>
-                <form className="form">
-                    {/* Grupo de entrada para o nome */}
-                    <div className="grupo-input border1">
-                        <label htmlFor="nome">Nome</label>
-                        <input type="text" id="nome" placeholder="Digite seu nome" />
-                    </div>
-
-                    {/* Grupo de entrada para o email */}
-                    <div className="grupo-input border1">
-                        <label htmlFor="email">E-mail</label>
-                        <input type="email" id="email" placeholder="Digite seu e-mail" />
-                    </div>
-
-                    {/* Grupo de entrada para a senha */}
-                    <div className="grupo-input border2">
-                        <label htmlFor="senha">Senha</label>
-                        <input type="password" id="senha" placeholder="Crie uma senha" />
-                    </div>
-
-                    {/* Grupo de entrada para a confirmação de senha */}
-                    <div className="grupo-input border2">
-                        <label htmlFor="confirmar-senha">Confirmar Senha</label>
-                        <input type="password" id="confirmar-senha" placeholder="Confirme sua senha" />
-                    </div>
-
-                    {/* Botões */}
-                    <div className="btns">
-                        <button type="button" className="botao-cadastro btn" onClick={cadastrar}>Cadastrar</button>
-                    </div>
-                </form>
-            </div>
-
+        <Grid container component="main" sx={{ height: '100vh' }}>
             {/* Seção de imagem */}
-            <div className="secao-imagem">
-                <img src="LOGO_TELA_DE_LOGIN.png" alt="Imagem de Cadastro" style={{ maxWidth: "100%", height: "100vh" }} />
-            </div>
-        </div>
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={6}
+                sx={{
+                    backgroundImage: `url(${Logo})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            />
+
+            {/* Seção de Cadastro */}
+            <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h4" gutterBottom>
+                        Cadastre-se
+                    </Typography>
+                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            id="nome"
+                            label="Nome"
+                            name="nome"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            autoComplete="name"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            id="email"
+                            label="E-mail"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            name="email"
+                            autoComplete="email"
+                        />
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            name="senha"
+                            label="Senha"
+                            type="password"
+                            id="senha"
+                            autoComplete="new-password"
+                        />
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            name="confirmar-senha"
+                            label="Confirmar Senha"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            id="confirmar-senha"
+                            autoComplete="new-password"
+                        />
+
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 2 }}>
+                            <Button
+                                type="button"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                onClick={handleSubmit}
+                                sx={{ mr: 2 }}
+                            >
+                                Cadastrar
+                            </Button>
+                        </Box>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
     );
 }
 
---
+++ my-app/src/controllers/loginController.js
@@ -0,0 +1,26 @@
+
+import axios from 'axios';
+
+// Função para enviar login ao backend
+export const login = async (email, password) => {
+  const loginData = {
+    email: email,
+    password: password,
+  };
+
+  try {
+    const response = await axios.post('http://localhost:8080/auth/login', loginData);
+
+    // Recebe o token JWT
+    const token = response.data.token;
+    debugger
+    
+    // Armazena o token no localStorage
+    localStorage.setItem('token', token);
+
+    return token; // Retorna o token ou outro valor conforme necessário
+  } catch (error) {
+    console.error('Erro no login:', error);
+    throw error; // Propaga o erro para ser tratado onde a função foi chamada
+  }
+};
